1398d041f597c0b5b2f495541752b539eab4d495
Marcus Baer <info@baer-media.com>
2013-03-13 18:44:10 +0100

    Simple GET and POST services

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var argv = require('optimist').argv,
     fs = require('fs');
 
 var trace_requests = false;
+var initial_save = true;
 
 var port = argv.port || 80;
 var thumbsFolder = argv.thumbs || 'viewer-images/thumbs';
@@ -13,15 +14,31 @@ var fullFolder = argv.full || 'viewer-images/full';
 var imagesMainFolder = getImagesMainFolder(thumbsFolder);
 var images = [];
 
+getDirectoryImages(thumbsFolder, function(file_with_path) {
+    var regex = new RegExp(imagesMainFolder + '\/');
+    images.push(getSImg(file_with_path.replace(regex,'')));
+});
+
 // setup data storage
 
 db.on('load', function() {
-    db.set('john', {eyes: 'blue'});
-    console.log('Added john, he has %s eyes.', db.get('john').eyes);
-
-    db.set('bob', {eyes: 'brown'}, function() {
-        console.log('User bob is now saved on disk.')
-    });
+    if (initial_save) {
+        var d = new Date(2013, 1, 5);
+        db.set('thumbs/Raspberry_Pi-595x446.jpg', {
+            'id': 'thumbs/Raspberry_Pi-595x446.jpg',
+            'title': 'A Raspberry Pi',
+            'src': 'thumbs/Raspberry_Pi-595x446.jpg',
+            'full': 'full/Raspberry_Pi-595x446.jpg',
+            'info': 'Model B',
+            'rotation': 0,
+            'excludetime': null,
+            'shottime': d.getTime(),
+            'shotplace': null
+        }, function() {
+            console.log('Image meta data to Raspberry is now saved on disk.')
+        });
+        console.log('Added Raspberry meta data, it is a %s.', db.get('thumbs/Raspberry_Pi-595x446.jpg').info);
+    }
 
     db.forEach(function(key, val) {
         console.log('Found key: %s, val: %j', key, val);
@@ -37,53 +54,89 @@ db.on('drain', function() {
 http.createServer(function (req, res) {
   var s = req.url.match(/(.*)\.(.{2,4})$/);
   var ext = (s && s[2]) ? s[2].toLowerCase() : null;
+
   if (trace_requests) {
       console.log(req.url + " [" + ext + "]");
   }
-  if (req.url.indexOf('/service')>=0) {
-	if (isAuthorized()) {
-		// service handling
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end(JSON.stringify(images));
-	} else {
-		res.writeHead(401, {'Content-Type': 'text/html'});
-		res.end('');
-	}
-  } else if (ext === 'jpg' || ext === 'jpeg') {
-    sendFile(res, imagesMainFolder+req.url, 'image/jpg');
-  } else if (ext === 'png') {
-    sendFile(res, imagesMainFolder+req.url, 'image/png');
-  } else if (ext === 'js') {
-    sendFile(res, 'static'+req.url, 'application/javascript');
-  } else if (ext === 'less') {
-    sendFile(res, 'static'+req.url, 'text/css');
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<html>' + 
-		'<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>' +
-		'<script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min.js"></script>' + 
-		'<script src="//cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.10/backbone-min.js"></script>' + 
-		'<script src="//cdnjs.cloudflare.com/ajax/libs/hammer.js/0.6.4/hammer.js"></script>'
-	);
-	if (isAuthorized()) {
-        res.write('<link rel="stylesheet/less" type="text/css" href="styles/basic.less" />');
-        res.write('<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.3.3/less.min.js"></script>');
-        res.write('<script src="js/jquery.simviewny.js"></script>');
-		res.write('<body><div id="image-count" style="display:none;">' + images.length + '</div></body></html>\n');
-	} else {
-        res.write('<link rel="stylesheet/less" type="text/css" href="styles/start.less" />');
-        res.write('<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.3.3/less.min.js"></script>');
-        res.write('<script src="js/jquery.start.js"></script>');
-		res.write('<body></body></html>\n');
-	}
-    res.end('\n');
+
+  if (isAuthorized() && req.method == 'POST') {
+      var body = '';
+      req.on('data', function (data) {
+          body += data;
+      });
+      req.on('end', function () {
+          req.body = parsePostBody(body);
+          // service handling POST
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          var sendData = { error: 'service not found' };
+          if (req.url.indexOf('/service')>=0) {
+              if (req.url.indexOf('/service/image/')>=0) {
+                  var id = req.url.replace(/\/service\/image\//,'');
+                  if (id === req.body.id) {
+                      db.set(req.body.id, req.body, function() {
+                          console.log('Image meta data for ' + req.body.id + ' is now saved on disk.')
+                      });
+                      sendData = req.body;
+                  } else {
+                      sendData = { error: 'id not matched'};
+                  }
+              }
+          }
+          res.end(JSON.stringify(sendData));
+      });
+  } else { // GET
+      if (req.url.indexOf('/service')>=0) {
+          if (isAuthorized()) {
+              // service handling GET
+              res.writeHead(200, {'Content-Type': 'text/plain'});
+              var sendData = { error: 'service not found' };
+              if (req.url === '/service/images') {
+                  sendData = images;
+              } else if (req.url.indexOf('/service/image/')>=0) {
+                  var id = req.url.replace(/\/service\/image\//,'');
+                  sendData = getSImg(id);
+              }
+              res.end(JSON.stringify(sendData));
+          } else {
+              res.writeHead(401, {'Content-Type': 'text/html'});
+              res.end('');
+          }
+      } else if (ext === 'jpg' || ext === 'jpeg') {
+          sendFile(res, imagesMainFolder+req.url, 'image/jpg');
+      } else if (ext === 'png') {
+          sendFile(res, imagesMainFolder+req.url, 'image/png');
+      } else if (ext === 'js') {
+          sendFile(res, 'static'+req.url, 'application/javascript');
+      } else if (ext === 'less') {
+          sendFile(res, 'static'+req.url, 'text/css');
+      } else {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write('<html>' +
+              '<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>' +
+              '<script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min.js"></script>' +
+              '<script src="//cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.10/backbone-min.js"></script>' +
+              '<script src="//cdnjs.cloudflare.com/ajax/libs/hammer.js/0.6.4/hammer.js"></script>'
+          );
+          if (isAuthorized()) {
+              res.write('<link rel="stylesheet/less" type="text/css" href="styles/basic.less" />');
+              res.write('<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.3.3/less.min.js"></script>');
+              res.write('<script src="js/jquery.simviewny.js"></script>');
+              res.write('<body><div id="image-count" style="display:none;">' + images.length + '</div></body></html>\n');
+          } else {
+              res.write('<link rel="stylesheet/less" type="text/css" href="styles/start.less" />');
+              res.write('<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.3.3/less.min.js"></script>');
+              res.write('<script src="js/jquery.start.js"></script>');
+              res.write('<body></body></html>\n');
+          }
+          res.end('\n');
+      }
   }
 
 }).listen(port);
 
 // some methods
 
-function getDirectoryFiles(directory, callback) {
+function getDirectoryImages(directory, callback) {
     fs.readdir(directory, function(err, files) {
         if (files && files.length>0) {
             files.forEach(function(file){
@@ -92,7 +145,7 @@ function getDirectoryFiles(directory, callback) {
                         callback(directory + '/' + file);
                     }
                     if(stats.isDirectory()) {
-                        getDirectoryFiles(directory + '/' + file, callback);
+                        getDirectoryImages(directory + '/' + file, callback);
                     }
                 });
             });
@@ -114,12 +167,38 @@ function isAuthorized () {
     return true;
 }
 
-getDirectoryFiles(thumbsFolder, function(file_with_path) {
-    var regex = new RegExp(imagesMainFolder + '\/');
-    images.push(file_with_path.replace(regex,''));
-});
+function parsePostBody (body) {
+    var post = {};
+    if (body.indexOf('{')==0) {
+        post = JSON.parse(body);
+    } else {
+        var parts = body.split('\n');
+        if (parts) {
+            parts.forEach(function(part){
+                var line = part.split('=');
+                post[line[0]] = line[1];
+            })
+        }
+    }
+    return post;
+}
 
 function getImagesMainFolder (folder) {
     var t = folder.split('/');
     return t[0];
+}
+
+function getSImg (filename) {
+    var meta = db.get(filename);
+    return {
+        'id': filename,
+        'title': meta.title || null,
+        'src': filename,
+        'full': meta.full || null,
+        'info': meta.info || null,
+        'rotation': meta.rotation || 0,
+        'excludetime': meta.excludetime || null,
+        'shottime': meta.shottime || null,
+        'shotplace': meta.shotplace || null
+    }
 }
--- static/js/jquery.simviewny.js
+++ static/js/jquery.simviewny.js
@@ -1,4 +1,25 @@
-﻿$(document).ready(function(){
+﻿var SImg = Backbone.Model.extend({
+    defaults: {
+        'id': null,
+        'title': null,
+        'src': null,
+        'full': null,
+        'info': null,
+        'rotation': 0,
+        'excludetime': null,
+        'shottime': null,
+        'shotplace': null
+    }
+});
+
+var SImgs = Backbone.Collection.extend({
+   model: SImg
+});
+
+var images = new SImgs();
+images.reset();
+
+$(document).ready(function(){
 
 	$(".image img").click(function(){
         $(".big img").attr("src", $(this).attr("src") ).show();
@@ -8,13 +29,37 @@
         $(this).attr("src", "").hide();
     });
 
-    $.get('/service/images', function(images){
-        images = JSON.parse(images);
-        var img_src = images.map(function(val) {
-            return '<div class="image"><img src="' + val + '" height="120" /></div>';
-        }).join('');
+/*
+     $.get('/service/image/thumbs/Raspberry_Pi-595x446.jpg', function(imgdata){
+        console.log("single get");
+        console.log(imgdata);
+     });
+*/
+
+    $.get('/service/images', function(imgdata){
+        images.reset();
+        imgdata = JSON.parse(imgdata);
+//        console.log(imgdata);
+        var img_src = '';
+        _.each(imgdata, function(img){
+            var i = new SImg(img);
+            images.push(i);
+            img_src = img_src + '<div class="image"><img src="' + img.src + '" height="120" /></div>';
+        });
         $("body").append("<h1>simviewny</h1><p>You have " + images.length + " image" + ((images.length>1) ? "s" : "") +  " stored.</p>");
         $("body").append(img_src);
+
+        // test a post request
+        $.ajax({
+            url: '/service/image/thumbs/Raspberry_Pi-595x446.jpg',
+            method: 'POST',
+            data: JSON.stringify(images.get('thumbs/Raspberry_Pi-595x446.jpg')),
+            success: function(imgdata){
+                console.log("image meta data saved for ...");
+                console.log(imgdata);
+            }
+        });
+
     });
 
 });
