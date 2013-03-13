ce865d8a525eb16bf129d7480bb72175245141ba
Marcus Baer <info@baer-media.com>
2013-03-13 13:44:34 +0100

    Use substacks 'optimist' for arguments and felixges 'node-dirty' for storage

--- server.js
+++ server.js
@@ -1,44 +1,38 @@
-﻿var http = require('http'),
+﻿#!/usr/bin/env node
+var argv = require('optimist').argv,
+    db = require('dirty')('db/images.db'),
+    http = require('http'),
     fs = require('fs');
 
 var trace_requests = false;
 
-function getDirectoryFiles(directory, callback) {
-    fs.readdir(directory, function(err, files) {
-        if (files && files.length>0) {
-            files.forEach(function(file){
-                fs.stat(directory + '/' + file, function(err, stats) {
-                    if(stats.isFile()) {
-                        callback(directory + '/' + file);
-                    }
-                    if(stats.isDirectory()) {
-                        getDirectoryFiles(directory + '/' + file, callback);
-                    }
-                });
-            });
-        }
-    });
-}
+var port = argv.port || 80;
+var thumbsFolder = argv.thumbs || 'viewer-images/thumbs';
+var fullFolder = argv.full || 'viewer-images/full';
 
-function sendFile (res, filename, contentType) {
-	fs.readFile(filename, function(err, data) {
-      if (err) {
-          throw err;
-      }
-      res.writeHead(200, {'Content-Type': contentType});
-      res.end(data);
-    });
-}
-
-function isAuthorized () {
-	return true;
-}
-
+var imagesMainFolder = getImagesMainFolder(thumbsFolder);
 var images = [];
 
-getDirectoryFiles('viewer-images/thumbs', function(file_with_path) {
-    images.push(file_with_path.replace(/viewer-images\//,''));
+// setup data storage
+
+db.on('load', function() {
+    db.set('john', {eyes: 'blue'});
+    console.log('Added john, he has %s eyes.', db.get('john').eyes);
+
+    db.set('bob', {eyes: 'brown'}, function() {
+        console.log('User bob is now saved on disk.')
+    });
+
+    db.forEach(function(key, val) {
+        console.log('Found key: %s, val: %j', key, val);
+    });
 });
+
+db.on('drain', function() {
+    console.log('All records are saved on disk now.');
+});
+
+// define server
 
 http.createServer(function (req, res) {
   var s = req.url.match(/(.*)\.(.{2,4})$/);
@@ -48,7 +42,7 @@ http.createServer(function (req, res) {
   }
   if (req.url.indexOf('/service')>=0) {
 	if (isAuthorized()) {
-		// service
+		// service handling
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end(JSON.stringify(images));
 	} else {
@@ -56,9 +50,9 @@ http.createServer(function (req, res) {
 		res.end('');
 	}
   } else if (ext === 'jpg' || ext === 'jpeg') {
-    sendFile(res, 'viewer-images'+req.url, 'image/jpg');
+    sendFile(res, imagesMainFolder+req.url, 'image/jpg');
   } else if (ext === 'png') {
-    sendFile(res, 'viewer-images'+req.url, 'image/png');
+    sendFile(res, imagesMainFolder+req.url, 'image/png');
   } else if (ext === 'js') {
     sendFile(res, 'static'+req.url, 'application/javascript');
   } else if (ext === 'less') {
@@ -85,4 +79,47 @@ http.createServer(function (req, res) {
     res.end('\n');
   }
 
-}).listen(80);
+}).listen(port);
+
+// some methods
+
+function getDirectoryFiles(directory, callback) {
+    fs.readdir(directory, function(err, files) {
+        if (files && files.length>0) {
+            files.forEach(function(file){
+                fs.stat(directory + '/' + file, function(err, stats) {
+                    if(stats.isFile()) {
+                        callback(directory + '/' + file);
+                    }
+                    if(stats.isDirectory()) {
+                        getDirectoryFiles(directory + '/' + file, callback);
+                    }
+                });
+            });
+        }
+    });
+}
+
+function sendFile (res, filename, contentType) {
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        res.writeHead(200, {'Content-Type': contentType});
+        res.end(data);
+    });
+}
+
+function isAuthorized () {
+    return true;
+}
+
+getDirectoryFiles(thumbsFolder, function(file_with_path) {
+    var regex = new RegExp(imagesMainFolder + '\/');
+    images.push(file_with_path.replace(regex,''));
+});
+
+function getImagesMainFolder (folder) {
+    var t = folder.split('/');
+    return t[0];
+}
