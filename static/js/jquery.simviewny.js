0d44974e82b2cad0d9a898e867dc7e3251419436
Marcus Baer <info@baer-media.com>
2013-03-13 18:58:19 +0100

    Click to enlarge image

--- static/js/jquery.simviewny.js
+++ static/js/jquery.simviewny.js
@@ -21,14 +21,6 @@ images.reset();
 
 $(document).ready(function(){
 
-	$(".image img").click(function(){
-        $(".big img").attr("src", $(this).attr("src") ).show();
-    });
-
-    $(".big img").click(function(){
-        $(this).attr("src", "").hide();
-    });
-
 /*
      $.get('/service/image/thumbs/Raspberry_Pi-595x446.jpg', function(imgdata){
         console.log("single get");
@@ -39,15 +31,27 @@ $(document).ready(function(){
     $.get('/service/images', function(imgdata){
         images.reset();
         imgdata = JSON.parse(imgdata);
-//        console.log(imgdata);
         var img_src = '';
         _.each(imgdata, function(img){
             var i = new SImg(img);
             images.push(i);
             img_src = img_src + '<div class="image"><img src="' + img.src + '" height="120" /></div>';
         });
+
+        // append all images to display
         $("body").append("<h1>simviewny</h1><p>You have " + images.length + " image" + ((images.length>1) ? "s" : "") +  " stored.</p>");
         $("body").append(img_src);
+        $("body").append('<div class="big"><img src="" style="display: none;" /></div>');
+
+        // attach click events to zoom in and out
+        $(".image img").click(function(){
+            var image = images.get($(this).attr("src"));
+            $(".big img").attr("src", image.get('full') ).show();
+        });
+
+        $(".big img").click(function(){
+            $(this).attr("src", "").hide();
+        });
 
         // test a post request
         $.ajax({
